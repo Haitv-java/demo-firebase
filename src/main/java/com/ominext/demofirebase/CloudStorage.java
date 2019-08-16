7d6f3da0f6a5154d5c6c8d22fd80a3c1f7734529
Trinh Hai <haitv@ominext.com>
2019-08-16 18:43:35 +0700

    init

---
+++ src/main/java/com/ominext/demofirebase/CloudStorage.java
@@ -0,0 +1,77 @@
+package com.ominext.demofirebase;// Imports the Google Cloud client library
+
+import com.google.api.gax.paging.Page;
+import com.google.auth.oauth2.ComputeEngineCredentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.Lists;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CloudStorage {
+    public static void main(String... args) throws Exception {
+        // Instantiates a client
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+
+        // The name for the new bucket
+        String bucketName = args[0];  // "my-new-bucket";
+
+        // Creates the new bucket
+        Bucket bucket = storage.create(BucketInfo.of(bucketName));
+        System.out.printf("Bucket %s created.%n", bucket.getName());
+
+        // firebase
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setDatabaseUrl("https://quick-start-pick.firebaseio.com/")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("restricted_access/secret_document");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object document = dataSnapshot.getValue();
+                System.out.println(document);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+    }
+
+    static void authImplicit() {
+        // If you don't specify credentials when constructing the client, the client library will
+        // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+
+        System.out.println("Buckets:");
+        Page<Bucket> buckets = storage.list();
+        for (Bucket bucket : buckets.iterateAll()) {
+            System.out.println(bucket.toString());
+        }
+    }
+
+    static void authExplicit(String jsonPath) throws IOException {
+        // You can specify a credential file by providing a path to GoogleCredentials.
+        // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath))
+                .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+
+        System.out.println("Buckets:");
+        Page<Bucket> buckets = storage.list();
+        for (Bucket bucket : buckets.iterateAll()) {
+            System.out.println(bucket.toString());
+        }
+    }
+}
---
+++ src/main/java/com/ominext/demofirebase/DemoFirebaseApplication.java
@@ -0,0 +1,16 @@
+package com.ominext.demofirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFirebaseApplication.class, args);
+
+
+    }
+
+
+}
